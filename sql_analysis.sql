2335812b38dbc29d254c4ca763e22df60bf52d5e
Charu Sanchihar <c.sanchihar@gmail.com>
2026-02-05 18:05:26 +0100

    adding another sql query for analysis

--- sql_analysis.sql
+++ sql_analysis.sql
@@ -51,6 +51,86 @@ select *
 from cte ) A 
 where rnk <= 5;
 
+###   or if we consider quantity 
+
+WITH cte AS
+(
+  SELECT
+    region,
+    product_id,
+    SUM(quantity) AS units_sold
+  FROM df_orders
+  GROUP BY region, product_id
+)
+SELECT *
+FROM (
+  SELECT *,
+         RANK() OVER (PARTITION BY region ORDER BY units_sold DESC) AS rnk
+  FROM cte
+) A
+WHERE rnk <= 5;
+
+######   with both quantity and sale price
+
+WITH cte AS
+(
+  SELECT
+    region,
+    product_id,
+    SUM(quantity) AS units_sold,
+    SUM(sale_price) AS total_sales
+  FROM df_orders
+  GROUP BY region, product_id
+)
+SELECT *
+FROM (
+  SELECT *,
+         row_number() OVER (PARTITION BY region ORDER BY units_sold DESC) AS rnk
+  FROM cte
+) A
+WHERE rnk <= 5;
+
+
+   
+## find month over month growth comparison for 2022 and 2023 sales eg : jan 2022 vs jan 2023
+
+-- checking the dictinct year present in table
+
+-- select  distinct year(order_date)
+-- from df_orders;
+
+-- we need the data in the below way 
+
+--       2022_sales  2023_sales
+-- jan
+-- ...
+-- dec 
+
+with cte as (
+select year(order_date) as order_year, month(order_date) as order_month, sum(sale_price) as sales
+from df_orders
+group by year(order_date),month(order_date)
+-- order by order_year,order_month
+)
+select order_month
+, sum(case when order_year = 2022 then sales else 0 end) as sales_2022
+, sum(case when order_year = 2023 then sales else 0 end) as sales_2023
+from cte 
+group by order_month
+order by order_month;
+
+
+#### 
+
+
+
+
+
+
+
+
+
+
 
 
 
